b2594754f19df4a65ecaec5b1cdd2ffda047f571
WangQianEve <eli_home@outlook.com>
2018-11-22 13:16:42 -0500

    welcome ready

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,22 @@
+var clip = true;
+window.onload = function() {
+    $('#main').css("clip-path", "url(#clip_q)");
+};
 window.addEventListener("scroll", function(e) {
-    let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-    console.log(scrollPercentage);
-    let font_size = 5 + scrollPercentage * 20;
+    let scrollPercentage = document.documentElement.scrollTop / (0.5 * document.documentElement.clientHeight);
+    if (scrollPercentage >= 1 && clip) {
+        clip = false;
+        console.log('clear clip path');
+        $("#main").css("clip-path", "");
+        return;
+    }
+    if (scrollPercentage <= 1 && !clip) {
+        clip = true;
+        $('#main').css("clip-path", "url(#clip_q)");
+        return;
+    }
+    let font_size = 5 + scrollPercentage * 800;
+    let y = 33 + scrollPercentage * 50;
     $("#clip_q text").css("font-size", font_size+"vw");
+    $("#clip_q text").attr("y", y+"vw");
 });
