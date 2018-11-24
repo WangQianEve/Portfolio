8e68926275ec22dbc03efb1c81f5b274067d9417
WangQianEve <eli_home@outlook.com>
2018-11-23 19:20:49 -0500

    welcome done

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,51 @@
-var clip = true;
+let ratio;
+let visible;
+let transform_origin;
+
 window.onload = function() {
-    $('#main').css("clip-path", "url(#clip_q)");
+    let q = $('#q_svg');
+    let q_pos = q.position();
+    let q_size = {height: q.height(), width: q.width()};
+    transform_origin = {x:q_pos.left+q_size.width*0.55, y:(q_pos.top+q_size.height*0.78)};
+    ratio = $(window).height()/6;
+    initVisibility();
 };
-window.addEventListener("scroll", function(e) {
+
+let initVisibility = function () {
     let scrollPercentage = document.documentElement.scrollTop / (0.5 * document.documentElement.clientHeight);
-    if (scrollPercentage >= 1 && clip) {
-        clip = false;
-        $("#main").css("clip-path", "");
-        return;
+    if (scrollPercentage >= 1) {
+        visible = false;
+        $('#welcome').hide();
+    } else {
+        visible = true;
     }
-    if (scrollPercentage <= 1 && !clip) {
-        clip = true;
-        $('#main').css("clip-path", "url(#clip_q)");
-        return;
+};
+
+let checkVisibility = function () {
+    let scrollPercentage = document.documentElement.scrollTop / (0.5 * document.documentElement.clientHeight);
+    if (scrollPercentage >= 1) {
+        if (visible) {
+            visible = false;
+            $('#welcome').hide();
+        }
+        return [false, scrollPercentage];
     }
-    // console.log(scrollPercentage);
-    let font_size = 5 + scrollPercentage * 800;
-    let y = 33 + scrollPercentage * 50;
-    $("#clip_q text").css("font-size", font_size+"vw");
-    $("#clip_q text").attr("y", y+"vw");
+    if (scrollPercentage <= 1 && !visible) {
+        $('#welcome').show();
+        visible = true;
+    }
+    return [true, scrollPercentage];
+};
+
+window.addEventListener("scroll", function(e) {
+    let vals = checkVisibility();
+    let cont = vals[0];
+    let scrollPercentage = vals[1];
+    if (!cont) {return;}
+    let scale = 1 + scrollPercentage * ratio;
+    $('#welcome').css({"transform":"scale("+scale+")", "transform-origin":transform_origin.x+"px "+transform_origin.y+"px"});
 });
 
-$('body').scrollspy({
-    target: '.bs-docs-sidebar',
-    offset: 40
-});
 $("#sidebar").affix({
     offset: {
         top: 60
